a1657bc5ed731ed018b48390eea3411db9138df0
BenjaminDaS <78204325+BenjaminDaS@users.noreply.github.com>
2022-06-09 10:34:47 +0200

    Add files via upload

--- RecherchePuissance4.java
+++ RecherchePuissance4.java
@@ -1,13 +1,13 @@
 package info1.sae;
 
 public class RecherchePuissance4 {
-	/** recherche Ã  chaque tour via un import de cette mÃ©thode si le jeu possÃ¨de 
-	 * quatre pions de la mÃªme couleur alignÃ©
-     * @param tableau 		tableau contenant les couleurs des pions placÃ©s Ã  leurs positions
-     * @param couleur       couleur du dernier pion placÃ©
-     * @param pion        	position du dernier pion placÃ©
-     * @return 				un boolean true si il existe quatre pions de la mÃªme couleur
-     * 						alignÃ©s
+	/** recherche à chaque tour via un import de cette méthode si le jeu possède 
+	 * quatre pions de la même couleur aligné
+     * @param tableau 		tableau contenant les couleurs des pions placés à leurs positions
+     * @param couleur       couleur du dernier pion placé
+     * @param pion        	position du dernier pion placé
+     * @return 				un boolean true si il existe quatre pions de la même couleur
+     * 						alignés
      */
 	public static boolean recherche(String[][] tableau, String couleur, int[] pionTest) {
 		
@@ -46,14 +46,11 @@ public class RecherchePuissance4 {
     		pion[0] -= 1;
     		pion[1] -= 1;
     	}
-    	for (int i = 0; i < 3 && pion[0] <= 6 && pion[1] <= 5;i ++) {
-    		if (tableau[pion[0]][pion[1]].equals(couleur)) {
-    			puissance4 = true;
-    			pion[0] += 1;
-    			pion[1] += 1;
-    		} else {
-    			return false;
-    		}
+    	if (pion[0] < 5 && pion[1] < 4) {
+    		puissance4 = tableau[pion[0]+1][pion[1]+1].equals(couleur);
+	    	puissance4 &= tableau[pion[0]+2][pion[1]+2].equals(couleur);
+	    	puissance4 &= tableau[pion[0]+3][pion[1]+3].equals(couleur);
+	    	return puissance4;
     	}
     	return puissance4;
     }
@@ -64,13 +61,11 @@ public class RecherchePuissance4 {
     	while(pion[0] != 0 && tableau[pion[0]-1][pion[1]].equals(couleur)) {
     		pion[0] -= 1;
     	}
-    	for (int i = 0; i < 3 && pion[0]+1 <= 6;i ++) {
-    		if (tableau[pion[0]+1][pion[1]].equals(couleur)) {
-    			pion[0] += 1;
-    			puissance4 = true;
-    		} else {
-    			return false;
-    		}
+    	if (pion[0] < 5) {
+    		puissance4 = tableau[pion[0]+1][pion[1]].equals(couleur);
+	    	puissance4 &= tableau[pion[0]+2][pion[1]].equals(couleur);
+	    	puissance4 &= tableau[pion[0]+3][pion[1]].equals(couleur);
+	    	return puissance4;
     	}
     	return puissance4;
     }
@@ -82,14 +77,11 @@ public class RecherchePuissance4 {
     		pion[0] += 1;
     		pion[1] -= 1;
     	}
-    	for (int i = 0; i < 3 && pion[0] != 0 && pion[1] != 5;i ++) {
-    		if (tableau[pion[0]-1][pion[1]+1].equals(couleur)) {
-    			pion[0] -= 1;
-    			pion[1] += 1;
-    			puissance4 = true;
-    		} else {
-    			return false;
-    		}
+    	if (pion[0] > 3 && pion[1] < 4) {
+    		puissance4 = tableau[pion[0]-1][pion[1]+1].equals(couleur);
+	    	puissance4 &= tableau[pion[0]-2][pion[1]+2].equals(couleur);
+	    	puissance4 &= tableau[pion[0]-3][pion[1]+3].equals(couleur);
+	    	return puissance4;
     	}
     	return puissance4;
     }
